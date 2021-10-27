444c2e8c15ca45c75cee1d492e8563b7d6b2425f
kotapatisravani <86467134+kotapatisravani@users.noreply.github.com>
2021-10-27 16:35:55 +0530

    Update example35.java

--- example35.java
+++ example35.java
@@ -1,7 +1,7 @@
-//import java.util.Scanner;
+/*Write a Java program to enter a number and print it in words.*/
 public class example35 {
 	public static void main(String[] args) {
-	    //Scanner scan=new Scanner(System.in);
+	    
 	    char ch=' ';
 	    for(int i=0;i<255;i++){
 	        System.out.println("The ascii value of "+(char)i+" = "+i);
