0ec6f99ab76a44eab72a4fb910ee6c2a5e83adc6
kotapatisravani <86467134+kotapatisravani@users.noreply.github.com>
2021-10-27 16:26:58 +0530

    Update example25.java

--- example25.java
+++ example25.java
@@ -1,13 +1,14 @@
+/*Write a Java program to print multiplication table of any number.*/
 import java.util.Scanner;
 public class example25 {
 	public static void main(String[] args) {
 	    Scanner scan=new Scanner(System.in);
 
-      System.out.println("enter table number:");
-       int n=scan.nextInt();
+             System.out.println("enter table number:");
+              int n=scan.nextInt();
        
-       for(int i=1;i<=10;i++){
-           System.out.println(n+" * "+i+" = "+(n*i));
-       }
-	}
+             for(int i=1;i<=10;i++){
+                   System.out.println(n+" * "+i+" = "+(n*i));
+              }
+	 }
 }	
