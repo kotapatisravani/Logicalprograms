fbd042c19a7ec0fb92b9d8e0e58055b18855ebae
kotapatisravani <86467134+kotapatisravani@users.noreply.github.com>
2021-10-27 16:10:04 +0530

    Update example18.java

--- example18.java
+++ example18.java
@@ -1,3 +1,4 @@
+/*Write a Java program to print all natural numbers in reverse (from n to 1). -     using while loop*/
 import java.util.Scanner;
 public class example18 {
 	public static void main(String[] args) {
